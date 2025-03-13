91b2cfbe2db559d24340a8caca7405482c4192d7
samuquellover <samuelescamp@gmail.com>
2025-03-13 21:57:03 +0100

    menu che mette in pausa

--- Eco Game/Assets/_GAME_/GameObjects/Player/Script/PauseMenu.cs
+++ Eco Game/Assets/_GAME_/GameObjects/Player/Script/PauseMenu.cs
@@ -4,49 +4,21 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
-    private bool notActive;
-    private float timer;
-    public float coolDown;
-    void Start()
-    {
-        notActive = true;
-        timer = coolDown;
-    }
 
-    // Update is called once per frame
     void Update()
     {
-
-        if (timer >= coolDown)
+        if (Input.GetKey(KeyCode.Escape))
         {
-            if (Input.GetKey(KeyCode.Escape))
-            {
-                if (notActive)
-                {
-                    pauseMenu.SetActive(true);
-                    notActive = false;
-                }
-                else if (!notActive)
-                {
-                    pauseMenu.SetActive(false);
-                    notActive = true;
-                }
-                timer = 0;
-            }
-            
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
         }
-        else
-        {
-            timer += Time.deltaTime;
-        }
-
 
     }
 
     public void resume ()
     {
         pauseMenu.SetActive(false);
-        notActive = true;
+        Time.timeScale = 1.0f;
     }
     public void options ()
     {
@@ -55,6 +27,7 @@ public class PauseMenu : MonoBehaviour
     public void home ()
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
     }
 
 }
