e4b34188e4c398c69493338fa9462281e7a89153
samuquellover <samuelescamp@gmail.com>
2025-03-29 03:16:23 +0100

    wowowo
    asd

--- Eco Game/Assets/_GAME_/GameObjects/DialogueBox/Scripts/DialogueActivator.cs
+++ Eco Game/Assets/_GAME_/GameObjects/DialogueBox/Scripts/DialogueActivator.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DialogueActivator : MonoBehaviour, IInteractable
 {
     [SerializeField] private DialogueObject dialogueObject;
+
     public void Interact (PlayerMovement player)
     {
         player.DialogueUI.ShowDialogue(dialogueObject);
